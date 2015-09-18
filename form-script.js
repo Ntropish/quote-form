da1c0abef947bcac2e33e3928ad56ece8fd370cb
NTrope <ju5t1n5t0n3@gmail.com>
2015-09-18 02:37:16 -0500

    Third 'final' commit
    Frequency changes caused bug later in form, has been fixed

--- form-script.js
+++ form-script.js
@@ -2,8 +2,6 @@
 (function(){
   function updatePrice(){
 
-
-
       if (structure === 'home') {
         jQuery('#quote-gen-story-input').attr('style', '');
       } else {
@@ -15,8 +13,9 @@
         number && water &&
         ((structure === 'home' && level) || (structure))
       ) {
-
+        // SET THE INITIAL COST HERE
         var total = 120;
+        // SET THE PER PANEL COSTS HERE
         if (number <= 8) {
           total += number * 14;
         } else if (number <= 20) {
@@ -27,13 +26,15 @@
           total += number * 7;
         } else if (number <= 70) {
           total += number * 5;
-        } else {
+        } else { // IF THE LOGIC GETS HERE THERE IS MORE THAN 70 PANELS
           jQuery('#quote-gen-price').text('Contact us for a commercial estimate.');
           return;
         }
+        // CHANGE THE WATER ADDITIONAL COST HERE
         if (water === 'no-access') {
           total += 120;
         }
+        // SET THE MOUNTING LEVEL COSTS HERE
         if (structure === 'home') {
           var levelCosts = {
             '1':0,
@@ -44,36 +45,32 @@
           total += levelCosts[level];
         }
         var finalText = '';
+        // CHANGE PACKAGE BOX PRICES HERE BY CHANGING THE 0.8, 0.82 etc. AT THE END OF THE LINE
         jQuery('button[name="frequency"]').filter('[value="bi-monthly"]').children('h4').text('$'+Math.ceil(total*0.8));
         jQuery('button[name="frequency"]').filter('[value="monthly"]').children('h4').text('$'+Math.ceil(total*0.82));
         jQuery('button[name="frequency"]').filter('[value="quarterly"]').children('h4').text('$'+Math.ceil(total*0.85));
         jQuery('button[name="frequency"]').filter('[value="semi-annually"]').children('h4').text('$'+Math.ceil(total*0.9));
         jQuery('button[name="frequency"]').filter('[value="annually"]').children('h4').text('$'+Math.ceil(total*0.95));
         jQuery('button[name="frequency"]').filter('[value="whenever"]').children('h4').text('$'+Math.ceil(total));
+        // CHANGE THE FINAL PRICE AND TEXT HERE
         if (frequency) {
           if (frequency==='bi-monthly') {
             total = Math.ceil(total * 0.8);
-
             finalText = 'Cost: $'+total+' per clean, twice a month.';
           } else if (frequency==='monthly') {
             total = Math.ceil(total * 0.82);
-
             finalText = 'Cost: $'+total+' per clean, once a month.';
           } else if (frequency==='quarterly') {
             total = Math.ceil(total * 0.85);
-
             finalText = 'Cost: $'+total+' per clean, four times per year.';
           } else if (frequency==='semi-annually') {
             total = Math.ceil(total * 0.9);
-
             finalText = 'Cost: $'+total+' per clean, two times per year.';
           } else if (frequency==='annually') {
             total = Math.ceil(total * 0.95);
-
             finalText = 'Cost: $'+total+' per clean, once per year.';
           } else if (frequency==='whenever') {
             total = Math.ceil(total * 1);
-
             finalText = 'Cost: $'+total+' per clean, as needed.';
           }
           jQuery('#quote-gen-price').text(finalText);
@@ -116,13 +113,13 @@
   });
 
   jQuery('.quote-gen-submit').on('click', function(){
-    var number = jQuery('input[name="panel-count"]').val();
-    var structure = jQuery('input[name="structure"]:checked').val();
-    var level = jQuery('input[name="level"]:checked').val();
-    var water = jQuery('input[name="water"]:checked').val();
-    var frequency = jQuery('input[name="frequency"]:checked').val();
-    var referrer = jQuery('input[name="referrer"]').val();
-    var details = jQuery('textarea[name="additional-details"]').val();
+    //var number = jQuery('input[name="panel-count"]').val();
+    //var structure = jQuery('input[name="structure"]:checked').val();
+    //var level = jQuery('input[name="level"]:checked').val();
+    //var water = jQuery('input[name="water"]:checked').val();
+    //var frequency = jQuery('input[name="frequency"]:checked').val();
+    //var referrer = jQuery('input[name="referrer"]').val();
+    //var details = jQuery('textarea[name="additional-details"]').val();
 
     var name = jQuery('.quote-gen-name').val();
     var address = jQuery('.quote-gen-address').val();
